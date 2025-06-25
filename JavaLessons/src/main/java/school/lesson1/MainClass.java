0e0459f3bca9939484d8a490c0dc159683ae1214
a.sharko <ann.sharkoshark@gmail.com>
2025-06-25 16:00:32 +0200

    First commit

--- JavaLessons/src/main/java/school/lesson1/MainClass.java
+++ JavaLessons/src/main/java/school/lesson1/MainClass.java
@@ -1,4 +1,51 @@
 package school.lesson1;
 
 public class MainClass {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 7;
+        int b = 3;
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("The sum is positive");
+        } else {
+            System.out.println("The sum is negative");
+        }
+    }
+
+    public static void printColor() {
+        int value = 47;
+
+        if (value <= 0) {
+            System.out.println("Red");
+        } else if (value <= 100) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 15;
+        int b = 13;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
 }
