5717ba191eefaddd7759f564fa81fd75e8fcb203
a.sharko <ann.sharkoshark@gmail.com>
2025-06-30 15:35:47 +0200

    changes made

--- JavaLessons/src/main/java/school/lesson7/StringPrinter.java
+++ JavaLessons/src/main/java/school/lesson7/StringPrinter.java
@@ -3,21 +3,25 @@ package school.lesson7;
 import java.util.Scanner;
 
 public class StringPrinter {
-    public static void printStringNTimes(String text, int n) {
-        for (int i = 0; i < n; i++) {
+
+    public static void printStringNTimes(String text, int number) {
+        int n = 0;
+        while (n < number) {
             System.out.println(text);
+            n++;
         }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the string to print: ");
-        String inputString = scanner.nextLine();
+        String text = scanner.nextLine();
 
-        System.out.print("Enter the number of times to print: ");
-        int times = scanner.nextInt();
+        System.out.print("Enter the number: ");
+        int number = scanner.nextInt();
 
-        printStringNTimes(inputString, times);
+        printStringNTimes(text, number);
 
         scanner.close();
     }
