bb2c8273ec4c17c4038d36683b9473a2eb76b5de
a.sharko <ann.sharkoshark@gmail.com>
2025-07-10 14:01:00 +0200

    changes made

--- JavaLessons/src/main/java/school/lesson10/Box.java
+++ JavaLessons/src/main/java/school/lesson10/Box.java
@@ -1,9 +1,10 @@
 package school.lesson10;
 import java.util.ArrayList;
 import java.util.List;
-public class Box <T extends Fruit>{
+public class Box <T extends Fruit> {
     private List<T> fruits = new ArrayList<>();
 
     public void addFruit(T fruit) {
         fruits.add(fruit);
+    }
 }
